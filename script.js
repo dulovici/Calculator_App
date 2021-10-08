f684893cd068dcf99d5eb76fd0866fe132e08f91
dulovici <igorx24@gmail.com>
2021-10-08 19:12:46 +0200

    bug fix / consecutive operations

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+(function(){
 'use strict'
 
 // DOM Elements
@@ -13,6 +13,7 @@ const reset = document.querySelector('.reset');
 const calc = document.querySelector('.equal');
 
 
+// Start State
 const calculator = {
     output: '0',
     firstOperand: '',
@@ -23,7 +24,6 @@ const calculator = {
 
 // FUNCTIONS
 function write() {
-    // console.log(this.className)
     if(!calculator.operator && this.className !== 'del'  && this.className !== 'operator') {
         calculator.firstOperand += this.value;
         calculator.output = calculator.firstOperand;
@@ -34,7 +34,7 @@ function write() {
             calculator.output = calculator.firstOperand;
             screen.textContent = calculator.output
         }
-    if(this.className === 'operator') {
+    if(this.className === 'operator' && !calculator.secondOperand) {
         calculator.operator = this.value;
         calculator.output = calculator.operator;
         screen.textContent = calculator.output
@@ -51,12 +51,15 @@ function write() {
             screen.textContent = calculator.output
         }
 
-        console.log(calculator)
+    if(this.className === 'operator' &&  calculator.secondOperand) {
+        calculate()
+        calculator.operator = this.value;
+    }
 }
 
 function arm() {
     calculator.firstOperand = calculator.output;
-    calculator.operator = null;
+    // calculator.operator = null;
     calculator.secondOperand = ''
     screen.textContent = calculator.output;
 }
@@ -69,23 +72,21 @@ function resetCalc() {
     screen.textContent = calculator.output
 }
 
-
-
 function calculate() {
     if(calculator.operator === '+') {
-        calculator.output = +calculator.firstOperand + +calculator.secondOperand;
+        calculator.output = `${+calculator.firstOperand + +calculator.secondOperand}`;
         arm()
     };
     if(calculator.operator === '-') {
-        calculator.output = +calculator.firstOperand - +calculator.secondOperand;
+        calculator.output = `${+calculator.firstOperand - +calculator.secondOperand}`;
         arm()  
     };
     if(calculator.operator === '*') {
-        calculator.output = +calculator.firstOperand * +calculator.secondOperand;
+        calculator.output = `${+calculator.firstOperand * +calculator.secondOperand}`;
         arm()
     };
     if(calculator.operator === '/') {
-        calculator.output = +calculator.firstOperand / +calculator.secondOperand;
+        calculator.output = `${+calculator.firstOperand / +calculator.secondOperand}`;
         arm() 
     };
 }
@@ -104,7 +105,6 @@ function setTheme() {
         body.className = '';
         body.classList.add('purple')
     }
-
 }
 
 // EVENTS
@@ -118,9 +118,4 @@ reset.addEventListener('click', resetCalc);
 
 themes.forEach(el => el.addEventListener('click', setTheme))
 
-
-
-
-
-// console.log(eval('3.5 + 3'))
-
+})();
