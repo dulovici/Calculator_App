a890737725a77c011244967e82f1d6652e350569
dulovici <igorx24@gmail.com>
2021-10-08 16:52:55 +0200

    v1.0

--- script.js
+++ script.js
@@ -1,2 +1,126 @@
 
-console.log('dev')
+'use strict'
+
+// DOM Elements
+// Theme
+const body = document.querySelector('body');
+const themes = document.querySelectorAll('.option')
+// Output
+const screen = document.querySelector('.output h1');
+// Input
+const btns = document.querySelectorAll('.container-1 button');
+const reset = document.querySelector('.reset');
+const calc = document.querySelector('.equal');
+
+
+const calculator = {
+    output: '0',
+    firstOperand: '',
+    operator: null,
+    secondOperand: ''
+}
+
+
+// FUNCTIONS
+function write() {
+    // console.log(this.className)
+    if(!calculator.operator && this.className !== 'del'  && this.className !== 'operator') {
+        calculator.firstOperand += this.value;
+        calculator.output = calculator.firstOperand;
+        screen.textContent = calculator.output
+    }
+        if(!calculator.operator && this.className === 'del') {
+            calculator.firstOperand = calculator.firstOperand.slice(0, -1);
+            calculator.output = calculator.firstOperand;
+            screen.textContent = calculator.output
+        }
+    if(this.className === 'operator') {
+        calculator.operator = this.value;
+        calculator.output = calculator.operator;
+        screen.textContent = calculator.output
+
+    }
+    if(calculator.operator && this.className !== 'del' && this.className !== 'operator') {
+        calculator.secondOperand += this.value;
+        calculator.output = calculator.secondOperand;
+        screen.textContent = calculator.output;
+    }
+        if(calculator.operator && this.className === 'del') {
+            calculator.secondOperand = calculator.secondOperand.slice(0, -1);
+            calculator.output = calculator.secondOperand;
+            screen.textContent = calculator.output
+        }
+
+        console.log(calculator)
+}
+
+function arm() {
+    calculator.firstOperand = calculator.output;
+    calculator.operator = null;
+    calculator.secondOperand = ''
+    screen.textContent = calculator.output;
+}
+
+function resetCalc() {
+    calculator.output = '0';
+    calculator.firstOperand = '';
+    calculator.operator = null;
+    calculator.secondOperand = '';
+    screen.textContent = calculator.output
+}
+
+
+
+function calculate() {
+    if(calculator.operator === '+') {
+        calculator.output = +calculator.firstOperand + +calculator.secondOperand;
+        arm()
+    };
+    if(calculator.operator === '-') {
+        calculator.output = +calculator.firstOperand - +calculator.secondOperand;
+        arm()  
+    };
+    if(calculator.operator === '*') {
+        calculator.output = +calculator.firstOperand * +calculator.secondOperand;
+        arm()
+    };
+    if(calculator.operator === '/') {
+        calculator.output = +calculator.firstOperand / +calculator.secondOperand;
+        arm() 
+    };
+}
+
+function setTheme() {
+    console.log(this.textContent)
+    if(this.textContent === '1') {
+        body.className = '';
+        body.classList.add('dark')
+    }
+    if(this.textContent === '2') {
+        body.className = '';
+        body.classList.add('light')
+    }
+    if(this.textContent === '3') {
+        body.className = '';
+        body.classList.add('purple')
+    }
+
+}
+
+// EVENTS
+btns.forEach(el => {
+    el.addEventListener('click', write)
+});
+
+calc.addEventListener('click', calculate);
+
+reset.addEventListener('click', resetCalc);
+
+themes.forEach(el => el.addEventListener('click', setTheme))
+
+
+
+
+
+// console.log(eval('3.5 + 3'))
+
